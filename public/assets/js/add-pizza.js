f79fdf967a585872b9be7f4947e66ac360cc0589
Pivotal Saint <108596467+PivotalSaint@users.noreply.github.com>
2022-11-13 10:12:07 -0600

    Update add-pizza.js

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -69,6 +69,3 @@ const handlePizzaSubmit = (event) => {
 
 $pizzaForm.addEventListener("submit", handlePizzaSubmit);
 $addToppingBtn.addEventListener("click", handleAddTopping);
-=======
-$pizzaForm.addEventListener('submit', handlePizzaSubmit);
-$addToppingBtn.addEventListener('click', handleAddTopping);
