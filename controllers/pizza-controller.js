d7bc1bdf67ad92f129f2c0fad33aea0544927fcc
PivotalSaint <licensedmt@att.net>
2022-11-13 12:48:56 -0600

    comments almost done

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,14 +5,26 @@ const { param } = require("../routes");
 const pizzaController = {
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: "comments",
+        select: "-_v",
+      })
+      .select("-_v")
+      .sort({ _id: -1 })
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
         res.status(400).json(err);
       });
   },
+
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
       .then((dbPizzaData) => {
         if (!dbPizzaData) {
           res.status(404).json({ message: "No pizza found with this id!" });
